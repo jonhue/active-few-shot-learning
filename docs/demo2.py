36a6b7688644dad3143db7f89e3ae5eb13a7f852
Jonas Hübotter <jonas.huebotter@gmail.com>
2024-02-19 18:21:42 +0100

    small refactoring

--- docs/demo2.py
+++ docs/demo2.py
@@ -1,5 +1,5 @@
 from afsl import ActiveDataLoader
 
-context_loader = ActiveDataLoader(data, target, batch_size=5)
+context_loader = ActiveDataLoader.initialize(data, target, batch_size=5)
 context = data[context_loader.next(model)]
 model.add_to_context(context)
