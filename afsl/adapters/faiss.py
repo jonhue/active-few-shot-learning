46e87455fb7787fc1e94fcffcd7cdab59a4ee19b
Jonas Hübotter <jonas.huebotter@gmail.com>
2024-05-14 11:14:25 +0200

    ImageNet evaluation

---
+++ afsl/adapters/faiss.py
@@ -0,0 +1,131 @@
+import faiss
+import torch
+import concurrent.futures
+import numpy as np
+from afsl import ActiveDataLoader
+from afsl.acquisition_functions.itl import ITL
+from torch.utils.data import Dataset as TorchDataset
+
+
+class Dataset(TorchDataset[torch.Tensor]):
+    def __init__(self, data: torch.Tensor):
+        self.data = data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index) -> torch.Tensor:
+        return self.data[index]
+
+
+class ITLSearcher:
+    """
+    Adapter for the [Faiss](https://github.com/facebookresearch/faiss) library.
+    First preselects a large number of candidates using Faiss, and then uses ITL to select the final results.
+
+    `ITLSearcher` can be used as a wrapper around a Faiss index object:
+
+    ```python
+    d = 768  # Dimensionality of the embeddings
+    index = faiss.IndexFlatIP(d)
+    index.add(embeddings)
+    itl = ITLSearcher(index)
+    indices = itl.search(query_embeddings, k=10)
+    ```
+    """
+
+    index: faiss.Index
+    force_nonsequential: bool = False
+    skip_itl: bool = False
+
+    def __init__(
+        self,
+        index: faiss.Index,
+        force_nonsequential: bool = False,
+        skip_itl: bool = False,
+    ):
+        self.index = index
+        self.force_nonsequential = force_nonsequential
+        self.skip_itl = skip_itl
+
+    def search(
+        self,
+        query: np.ndarray,
+        k: int,
+        k_mult: float = 100.0,
+        mean_pooling: bool = False,
+        threads: int = 1,
+    ) -> np.ndarray:
+        r"""
+        :param query: Query embedding (of shape $m \times d$), comprised of $m$ individual embeddings.
+        :param k: Number of results to return.
+        :param k_mult: `k * k_mult` is the number of results to pre-sample before executing ITL.
+        :param mean_pooling: Whether to use the mean of the query embeddings.
+        :param threads: Number of threads to use.
+
+        :return: Array of selected indices (of length $k$).
+        """
+        return self.batch_search(
+            queries=np.array([query]),
+            k=k,
+            k_mult=k_mult,
+            mean_pooling=mean_pooling,
+            threads=threads,
+        )[0]
+
+    def batch_search(
+        self,
+        queries: np.ndarray,
+        k: int,
+        k_mult: float = 100.0,
+        mean_pooling: bool = False,
+        threads: int = 1,
+    ) -> np.ndarray:
+        r"""
+        :param queries: $n$ query embeddings (of combined shape $n \times m \times d$), each comprised of $m$ individual embeddings.
+        :param k: Number of results to return.
+        :param k_mult: `k * k_mult` is the number of results to pre-sample before executing ITL.
+        :param mean_pooling: Whether to use the mean of the query embeddings.
+        :param threads: Number of threads to use.
+
+        :return: Array of selected indices (of shape $n \times k$).
+        """
+        assert k_mult > 1
+
+        queries = queries.astype("float32")
+        n, m, d = queries.shape
+        assert d == self.index.d
+        mean_queries = np.mean(queries, axis=1)
+
+        faiss.omp_set_num_threads(threads)
+        D, I, V = self.index.search_and_reconstruct(mean_queries, int(k * k_mult))
+
+        if self.skip_itl:
+            return I[:, :k]
+
+        def engine(i: int) -> np.ndarray:
+            dataset = Dataset(torch.tensor(V[i]))
+            target = torch.tensor(
+                queries[i] if not mean_pooling else mean_queries[i].reshape(1, -1)
+            )
+            acquisition_function = ITL(
+                target=target,
+                num_workers=threads,
+                subsample=False,
+                force_nonsequential=self.force_nonsequential,
+            )
+            sub_indexes = ActiveDataLoader(
+                dataset=dataset,
+                batch_size=k,
+                acquisition_function=acquisition_function,
+            ).next()
+            return np.array(I[i][sub_indexes])
+
+        result = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+            futures = []
+            for i in range(n):
+                futures.append(executor.submit(engine, i))
+            for future in concurrent.futures.as_completed(futures):
+                result.append(future.result())
+        return np.array(result)
---
+++ examples/fine_tuning/imagenet/model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from afsl.embeddings.classification import HallucinatedCrossEntropyEmbedding
+
+
+class EfficientNet(nn.Module):
+    def __init__(self, output_dim: int):
+        super(EfficientNet, self).__init__()
+
+        self.model = torch.hub.load(
+            "NVIDIA/DeepLearningExamples:torchhub",
+            "nvidia_efficientnet_b0",
+            pretrained=True,
+        )
+
+        # Freeze all layers except the penultimate layer
+        for param in self.model.parameters():
+            param.requires_grad = False
+        for param in self.model.features[-1].parameters():
+            param.requires_grad = True
+
+        # Replace the last linear layer
+        self.fc = self.model.classifier.fc
+        self.model.classifier.fc = torch.nn.Identity()
+
+    @property
+    def final_layer(self):
+        return self.fc
+
+    def logits(self, x):
+        x = self.model(x)
+        return x
+
+    def forward(self, x):
+        x = self.logits(x)
+        x = self.fc(x)
+        return x
+
+    def predict(self, x):
+        outputs = self(x)
+        _, predicted = torch.max(outputs.data, dim=1)
+        return predicted
+
+    # def reset(self):
+    #     self.fc.reset_parameters()
+
+
+class EfficientNetWithHallucinatedCrossEntropyEmbedding(
+    EfficientNet, HallucinatedCrossEntropyEmbedding
+):
+    pass
+
+
+class EfficientNetWithLastLayerEmbedding(EfficientNet):
+    def embed(self, x):
+        return self.logits(x)
